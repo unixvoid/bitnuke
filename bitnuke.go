507aa88225dc6784465b0d7fee704ed136cb0ad1
Matthew Faltys <mfaltys@gmail.com>
2015-12-24 16:46:33 -0600

    working base model and strategy

--- bitnuke.go
+++ bitnuke.go
@@ -1,31 +1,48 @@
 package main
 
 import (
-	//"fmt"
-	//"github.com/gorilla/mux"
+	"crypto/md5"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
 	"net/http"
+	//"strconv"
+	//"time"
 )
 
-// working
-//func main() {
-//	router := mux.NewRouter()
-//	stat := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
-//	router.PathPrefix("/static/").Handler(stat)
+/*
+//=====================================
+// general strategy:
+// we take in a file, the filename is a hashed random string.
+// the file is stored with its filename as the hased string.
+// the random string (token) is returned back to the user.
 //
-//	http.Handle("/", router)
-//	http.ListenAndServe(":80", router)
-//}
+// now when the user wants to retrive the file, he puts in the
+// token (random string from earlier). his request is hashed and
+// the stored has is returned. ez
+//=====================================
+*/
 
 func main() {
-	//repoFrontend := "./tmpnuke/"
-	//http.Handle("/", http.FileServer(http.Dir(repoFrontend)))
-	http.Handle("/", handlerstatic)
-	err := http.ListenAndServe(":80", nil)
-	if nil != err {
-		panic(err)
-	}
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/{fdata}", handlerdynamic).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8888", router))
 }
 
-func handlerstatic(w http.ResponseWriter, r *http.Request) {
-	http.FileServer(http.Dir("./tmpnuke/"))
+func handlerdynamic(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fdata := vars["fdata"]
+
+	//hash the token that is passed
+	hash := md5.Sum([]byte(fdata))
+	hashstr := fmt.Sprintf("%x", hash)
+
+	//serve up the hashed token if it exists
+	token(w, r, hashstr)
+}
+
+func token(w http.ResponseWriter, r *http.Request, data string) {
+	log.Printf("Responsing to", data)
+	item := fmt.Sprintf("./tmpnuke/%s", data)
+	http.ServeFile(w, r, item)
 }
