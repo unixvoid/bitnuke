ce4317bd2c94fec46d2b1ed1ab03a67037427ed2
Matthew Faltys <mfaltys@gmail.com>
2015-12-26 00:09:11 -0600

    semi-working together

--- bitnuke.go
+++ bitnuke.go
@@ -4,14 +4,16 @@ import (
 	"crypto/md5"
 	"fmt"
 	"github.com/gorilla/mux"
+	"html/template"
+	"io"
 	//"io/ioutil"
 	"crypto/rand"
 	//"encoding/base64"
 	"log"
 	"net/http"
 	"os"
-	//"strconv"
-	//"time"
+	"strconv"
+	"time"
 )
 
 /*
@@ -30,6 +32,7 @@ import (
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/{fdata}", handlerdynamic).Methods("GET")
+	router.HandleFunc("/upload", upload)
 	log.Fatal(http.ListenAndServe(":8888", router))
 }
 
@@ -41,7 +44,7 @@ func handlerdynamic(w http.ResponseWriter, r *http.Request) {
 	hash := md5.Sum([]byte(fdata))
 	hashstr := fmt.Sprintf("%x", hash)
 
-	token := randStr(8)
+	//token := randStr(8)
 	//log.Printf(token)
 
 	// serve up the hashed token if it exists
@@ -54,6 +57,43 @@ func handlerdynamic(w http.ResponseWriter, r *http.Request) {
 	// if data does not exist
 	if _, err := os.Stat(path); err != nil {
 		fmt.Fprintf(w, "token not found")
+	}
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("upload.gtpl")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, _, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		//fmt.Fprintf(w, "%v", handler.Header)
+
+		// generate token and hash to save
+		token := randStr(8)
+		fmt.Fprintf(w, "%v", token)
+		hash := md5.Sum([]byte(token))
+		hashstr := fmt.Sprintf("%x", hash)
+		fmt.Println(token)
+
+		f, err := os.OpenFile("./tmpnuke/"+hashstr, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
 	}
 }
 
