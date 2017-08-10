dd2314f46c67c4cee6681c68d24dcd50d64c059d
Matthew Faltys <mfaltys@gmail.com>
2017-08-10 14:41:59 -0500

    Clean up code/documentation

--- bitnuke/link_compressor.go
+++ bitnuke/link_compressor.go
@@ -16,10 +16,12 @@ func linkcompressor(w http.ResponseWriter, r *http.Request, redisClient *redis.C
 	if err != nil {
 		glogger.Error.Println("error during form parse")
 	}
-	content := r.PostFormValue("link")
-	page := fmt.Sprintf("<html><head><meta http-equiv=\"refresh\" content=\"0;URL=%s\"></head></html>", content)
+
+	// generate redirect html
+	page := fmt.Sprintf("<html><head><meta http-equiv=\"refresh\" content=\"0;URL=%s\"></head></html>", r.PostFormValue("link"))
 	glogger.Debug.Println("link created")
 	content64Str := base64.StdEncoding.EncodeToString([]byte(page))
+
 	// generate token and hash it to store in db
 	token := tokenGen(config.Bitnuke.LinkTokenSize, redisClient)
 	hash := sha3.Sum512([]byte(token))
@@ -29,6 +31,7 @@ func linkcompressor(w http.ResponseWriter, r *http.Request, redisClient *redis.C
 	redisClient.Set(hashstr, content64Str, 0).Err()
 	redisClient.Set(fmt.Sprintf("fname:%s", hashstr), "bitnuke:link", 0).Err()
 	redisClient.Expire(hashstr, (config.Bitnuke.TTL * time.Hour)).Err()
+
 	// return token to redisClient
 	w.Header().Set("compressor", token)
 	fmt.Fprintf(w, "%s", token)
