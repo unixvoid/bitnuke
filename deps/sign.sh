deb0b2809bbb3d285728f6eb2aba55ec369f1814
Matthew Faltys <mfaltys@gmail.com>
2017-09-15 10:42:35 -0500

    Fix sign to accept new filename

--- deps/sign.sh
+++ deps/sign.sh
@@ -4,5 +4,5 @@
 	--batch --yes \
 	--no-default-keyring --armor \
 	--secret-keyring ./unixvoid.sec --keyring ./unixvoid.pub \
-	--output bitnuke-api-latest-linux-amd64.aci.asc \
-	--detach-sig bitnuke-api-latest-linux-amd64.aci
+	--output bitnuke-api-0.20.1-linux-amd64.aci.asc \
+	--detach-sig bitnuke-api-0.20.1-linux-amd64.aci
