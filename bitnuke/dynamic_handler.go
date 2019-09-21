999faf389580bb172b47de178515de9bbd1d65bf
Matthew Faltys <mfaltys@d3banking.com>
2019-09-21 15:59:26 +0000

    Revert fix

--- bitnuke/dynamic_handler.go
+++ bitnuke/dynamic_handler.go
@@ -81,7 +81,7 @@ func handlerdynamic(w http.ResponseWriter, r *http.Request, redisClient *redis.C
 
 		// dont add the filename header to links
 		if string(filename) != "bitnuke:link" {
-			finalFname := fmt.Sprintf("attachment; filename=%s", filename)
+			finalFname := fmt.Sprintf("INLINE; filename=%s", filename)
 			w.Header().Set("Content-Disposition", finalFname)
 		}
 		http.ServeFile(w, r, "tmpfile")
